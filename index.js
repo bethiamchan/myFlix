5005d49ce5882744ce60aeafb0d3777089f30f43
Bethia Chan <bethiamchan@gmail.com>
2020-10-13 11:43:14 -0500

    Updated documentation

--- index.js
+++ index.js
@@ -1,78 +1,78 @@
 const express = require('express'),
-    morgan = require('morgan');
+	morgan = require('morgan');
 const app = express();
 
 let movies = [
-    {
-        title: 'Pride and Prejudice',
-        director: 'Joe Wright',
-        genre: ['Drama', 'Romance'],
-        released: 2005,
-        starring: ['Keira Knightley', 'Matthew Macfadyen', 'Rosamund Pike']
-    },
-    {
-        title: 'Emma',
-        director: 'Autumn de Wilde',
-        genre: ['Comedy', 'Drama', 'Romance'],
-        released: 2020,
-        starring: ['Anya Taylor-Joy', 'Mia Goth', 'Johnny Flynn', 'Bill Nighy']
-    },
-    {
-        title: 'Sense and Sensibility',
-        director: 'Ang Lee',
-        genre: ['Drama', 'Romance'],
-        released: 1995,
-        starring: ['Emma Thompson', 'Kate Winslet', 'Hugh Grant', 'Alan Rickman']
-    },
-    {
-        title: 'Love and Friendship',
-        director: 'Whit Stillman',
-        genre: ['Comedy', 'Drama', 'Romance'],
-        released: 2016,
-        starring: ['Kate Beckinsale', 'Morfydd Clark', 'Chloe Sevigny']
-    },
-    {
-        title: 'Little Women',
-        director: 'Greta Gerwig',
-        genre: ['Drama', 'Romance'],
-        released: 2019,
-        starring: ['Saoirse Ronan', 'Emma Watson', 'Florence Pugh', 'Laura Dern', 'Timothee Chalamet', 'Meryl Streep']
-    },
-    {
-        title: 'Enola Holmes',
-        director: 'Harry Bradbeer',
-        genre: ['Adventure', 'Drama', 'Crime'],
-        released: 2020,
-        starring: ['Millie Bobby Brown', 'Henry Cavill', 'Sam Claflin', 'Helena Bonham Carter']
-    },
-    {
-        title: 'Ladies in Black',
-        director: 'Bruce Beresford',
-        genre: ['Comedy', 'Drama', 'Romance'],
-        released: 2018,
-        starring: ['Julia Ormond', 'Angourie Rice', 'Rachael Taylor']
-    },
-    {
-        title: 'Belle',
-        director: 'Amma Asante',
-        genre: ['Biography', 'Drama', 'Romance'],
-        released: 2013,
-        starring: ['Gugu Mbatha-Raw', 'Matthew Goode', 'Emily Watson']
-    },
-    {
-        title: 'The Help',
-        director: 'Tate Taylor',
-        genre: ['Drama'],
-        released: 2011,
-        starring: ['Emma Stone', 'Viola Davis', 'Octavia Spencer']
-    },
-    {
-        title: 'Hidden Figures',
-        director: 'Theodore Melfi',
-        genre: ['Biography', 'Drama', 'History'],
-        released: 2016,
-        starring: ['Taraji P. Henson', 'Octavia Spencer', 'Janelle Monae', 'Kevin Costner', 'Jim Parsons', 'Mahershala Ali']
-    },
+	{
+		title: 'Pride and Prejudice',
+		director: 'Joe Wright',
+		genre: ['Drama', 'Romance'],
+		released: 2005,
+		starring: ['Keira Knightley', 'Matthew Macfadyen', 'Rosamund Pike'],
+	},
+	{
+		title: 'Emma',
+		director: 'Autumn de Wilde',
+		genre: ['Comedy', 'Drama', 'Romance'],
+		released: 2020,
+		starring: ['Anya Taylor-Joy', 'Mia Goth', 'Johnny Flynn', 'Bill Nighy'],
+	},
+	{
+		title: 'Sense and Sensibility',
+		director: 'Ang Lee',
+		genre: ['Drama', 'Romance'],
+		released: 1995,
+		starring: ['Emma Thompson', 'Kate Winslet', 'Hugh Grant', 'Alan Rickman'],
+	},
+	{
+		title: 'Love and Friendship',
+		director: 'Whit Stillman',
+		genre: ['Comedy', 'Drama', 'Romance'],
+		released: 2016,
+		starring: ['Kate Beckinsale', 'Morfydd Clark', 'Chloe Sevigny'],
+	},
+	{
+		title: 'Little Women',
+		director: 'Greta Gerwig',
+		genre: ['Drama', 'Romance'],
+		released: 2019,
+		starring: ['Saoirse Ronan', 'Emma Watson', 'Florence Pugh', 'Laura Dern', 'Timothee Chalamet', 'Meryl Streep'],
+	},
+	{
+		title: 'Enola Holmes',
+		director: 'Harry Bradbeer',
+		genre: ['Adventure', 'Drama', 'Crime'],
+		released: 2020,
+		starring: ['Millie Bobby Brown', 'Henry Cavill', 'Sam Claflin', 'Helena Bonham Carter'],
+	},
+	{
+		title: 'Ladies in Black',
+		director: 'Bruce Beresford',
+		genre: ['Comedy', 'Drama', 'Romance'],
+		released: 2018,
+		starring: ['Julia Ormond', 'Angourie Rice', 'Rachael Taylor'],
+	},
+	{
+		title: 'Belle',
+		director: 'Amma Asante',
+		genre: ['Biography', 'Drama', 'Romance'],
+		released: 2013,
+		starring: ['Gugu Mbatha-Raw', 'Matthew Goode', 'Emily Watson'],
+	},
+	{
+		title: 'The Help',
+		director: 'Tate Taylor',
+		genre: ['Drama'],
+		released: 2011,
+		starring: ['Emma Stone', 'Viola Davis', 'Octavia Spencer'],
+	},
+	{
+		title: 'Hidden Figures',
+		director: 'Theodore Melfi',
+		genre: ['Biography', 'Drama', 'History'],
+		released: 2016,
+		starring: ['Taraji P. Henson', 'Octavia Spencer', 'Janelle Monae', 'Kevin Costner', 'Jim Parsons', 'Mahershala Ali'],
+	},
 ];
 
 //Log requests using Morgan middleware
@@ -80,7 +80,7 @@ app.use(morgan('common'));
 
 //Requests
 app.get('/', (req, res) => {
-    res.send('Welcome to myFlix!');
+	res.send('Welcome to myFlix!');
 });
 
 app.get('/movies', (req, res) => {
@@ -88,7 +88,7 @@ app.get('/movies', (req, res) => {
 });
 
 app.get('/movies/:title', (req, res) => {
-    res.send('Successful GET request returning information about a movie based on title.');
+	res.send('Successful GET request returning information about a movie based on title.');
 });
 
 app.get('/movies/genres/:genre', (req, res) => {
@@ -104,15 +104,15 @@ app.post('/users', (req, res) => {
 });
 
 app.put('/users/:username', (req, res) => {
-	res.send('Successful PUT request updating a user\'s information.');
+	res.send("Successful PUT request updating a user's information.");
 });
 
 app.post('/users/:username/movies/:movieID', (req, res) => {
-	res.send('Successful POST request adding a movie to user\'s favorites.');
+	res.send("Successful POST request adding a movie to user's favorites.");
 });
 
 app.delete('/users/:username/movies/:movieID', (req, res) => {
-	res.send('Successful DELETE request removing a movie from user\'s favorites.');
+	res.send("Successful DELETE request removing a movie from user's favorites.");
 });
 
 app.delete('/users/:username', (req, res) => {
@@ -123,11 +123,9 @@ app.use(express.static('public'));
 
 //Error handling
 app.use((err, req, res, next) => {
-    console.error(err.stack);
-    res.status(500).send('Something broke!');
+	console.error(err.stack);
+	res.status(500).send('Something broke!');
 });
 
 //listen for requests
-app.listen(8080, () =>
-    console.log('Your app is listening on port 8080.')
-);
+app.listen(8080, () => console.log('Your app is listening on port 8080.'));
