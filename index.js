38b4a07cfbf7e616d7290d664ab5f274ad9c1c50
Bethia Chan <bethiamchan@gmail.com>
2020-10-29 11:50:05 -0500

    Update Connection URI to Mongo Atlas

--- index.js
+++ index.js
@@ -10,7 +10,8 @@ const { check, validationResult } = require('express-validator');
 const Movies = Models.Movie;
 const Users = Models.User;
 
-mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 mongoose.set('useFindAndModify', false);
 
 const app = express();
